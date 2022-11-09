68f25a04d6bff55c82db9d70efcb52eaccdb5a8f
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2022-11-09 14:32:18 +0000

    Player collision with ground
    player now properly collides with ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,34 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    //horizontal speed
     public float playerSpeed = 10f;
+    //the jump height
     public float playerJumpHeight = 10f;
-
-    private bool grounded = false;
     private Animator anim;
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    [SerializeField] LayerMask levelLayer;
 
     void Awake() {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>(); // for animations
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update(){
-        grounded = Physics2D.Raycast(transform.position, Vector2.down, 0.5f); // raycast down to check if player is standing on something
 
         //make player move on the x axes
         rb.velocity = new Vector2(Input.GetAxis("Horizontal")*playerSpeed,rb.velocity.y);
 
-        // so the player doesnt rotate since it has rigidbody: dynamic
-        transform.rotation = Quaternion.identity;
-
+        
         if (Input.GetKey(KeyCode.Space)){
-            Debug.Log("GROUNDED: "+grounded);
-            if (grounded){
-                rb.velocity = new Vector2(rb.velocity.x, playerJumpHeight);
+            if (isGrounded()){
+                rb.velocity = new Vector2(rb.velocity.x, playerJumpHeight); // apply vertical velocity;
             }
         }
-        
+
+    }
+
+    private bool isGrounded(){
+        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,  Vector2.down, 0.1f, levelLayer); // raycast down to check if player is standing on something;    NOTE layer mask is the layer that it will recognise when it is being hit
+        return raycast.collider != null;
     }
 }
