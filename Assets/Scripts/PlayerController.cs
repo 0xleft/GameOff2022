7e4a6b4906e564b57999eb35ccc8186cad07d2d8
Adomas <107749872+AdomasDauda@users.noreply.github.com>
2022-11-10 09:19:09 +0100

    Final fix for player wall and changed level
    Cancel player velocity to wall by checking what side wall player is hugging.
    
    Player fix now cannot double jump on wall since the earlier boxcast was full size it would sometimes trigger the inGrounded method when it was hitting a wall.
    
    Level changed from tilemap since its colision is inconsistant so now we build our own level from modeled parts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,16 +19,23 @@ public class PlayerController : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
+    // values only modified in the update method
     void Update(){
 
         // if player on the wall it is a glitch where he will just float when huggin the wall
-            // move player on the x axis
+        // move player on the x axis
         rb.velocity = new Vector2(Input.GetAxis("Horizontal")*playerSpeed,rb.velocity.y);
         
-        if (onWall() && !isGrounded()){ // we should call on wall animation TODO make so you are able to jump of the wall
+        if (onLeftWall() && rb.velocity.x < 0){ // on left wall and moving left
+            // cancel the movement to the left
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
+        if (onRightWall()){ // on right wall and moving right
+            // cancel the movement to the right
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        
         if (Input.GetKey(KeyCode.Space)){
             if (isGrounded()){
                 rb.velocity = new Vector2(rb.velocity.x, playerJumpHeight); // apply vertical velocity;
@@ -38,11 +45,22 @@ public class PlayerController : MonoBehaviour
 
     // check if player is on the ground 
     private bool isGrounded(){
-        RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,  Vector2.down, 0.4f, levelLayer); //NOTE layer mask is the layer that it will recognise when it is being hit
-        return raycast.collider != null;
+        //NOTE layer mask is the layer that it will recognise when it is being hit
+        return Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, 0.6f, levelLayer).collider != null;
     }
 
-    // check if player is huggin a wall
+    // check if player is huggin left wall
+    private bool onLeftWall(){
+        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,  Vector2.left, 0.05f, levelLayer).collider != null;
+    }
+
+    // is player huggin right wall
+    private bool onRightWall(){
+        // check if player is huggin right wall
+        return Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,  Vector2.right, 0.05f, levelLayer).collider != null;
+    }
+
+    // is player huggin a wall
     private bool onWall(){
         // check left side
         RaycastHit2D raycast = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0,  Vector2.left, 0.05f, levelLayer);
